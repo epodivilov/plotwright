b15f8960802a56e85ca6bec6b6fbcc334d98aee3
Evgeniy Podivilov <evgeniy.podivilov@gmail.com>
2023-05-17 07:15:31 +0100

    feat: :sparkles: create util to define configuration

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,24 @@ export * from "./core";
 export * from "./core/page-object";
 export { defineConfig, devices } from "@playwright/test";
 
-export type Configuration = {
-  mountebank: {
-    imposters: string[];
-  };
-  playwright: PlaywrightTestConfig;
+type MountebankOptions = {
+  host?: string;
+  port?: number;
+  logLevel?: "debug" | "info" | "warn" | "error";
+  debug?: boolean;
+  allowInjection?: boolean;
 };
+
+export type Configuration = {
+  mountebank?: MountebankOptions & {
+    imposters?: string[];
+  };
+  playwright?: PlaywrightTestConfig;
+};
+
+export function makeConfig(config: Configuration): Configuration {
+  return {
+    mountebank: config.mountebank || {},
+    playwright: config.playwright || {},
+  };
+}
